2b7e88b36e52c08523355557639a762170d47ca5
Brent Danley <brentdanley@gmail.com>
2021-06-02 18:57:17 -0400

    Updates site title.

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Brent Danley'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Brent Danley - Code Portfolio'
 
 export default function Layout({ children, home }) {
   return (
