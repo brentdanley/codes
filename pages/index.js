8eeada943f3c0fa09cbd4115442fc60ca888f38d
Brent Danley <brentdanley@gmail.com>
2021-06-28 10:22:51 -0400

    Removes paragraph from homepage.

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hi, I'm Brent. I'm a software engineer living in Southern Maine. I'm glad you're here.</p>
         <p>This site is a portfolio of my work, but also serves as a blog dedicated primarily to professional interests.</p>
-        <p>The intended audience is prospective employers and any front-end developer seeking advice, tips, how-tos, and best practices. This site is for you, but it also gives me a platform to develop and improve skills I think are important but that I don't do as a regular part of my job. Showing someone how to do something is a great research motivator and a tool for me to guage my own level of knowledge in the myriad topics covered.</p>
         <p>Look around. Provide constructive feedback. Hit me up on social.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
